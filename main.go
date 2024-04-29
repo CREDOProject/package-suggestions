d2ae31af12c8b1905fa76c520b24f0cf56552d71
Eliseo Martelli <me@eliseomartelli.it>
2024-04-29 15:35:44 +0200

    chore: restructure

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path"
 	"regexp"
 )
 
@@ -27,28 +28,36 @@ type OutputSuggestions struct {
 }
 
 func main() {
-	jsonFile, err := os.Open("./suggestion.json")
+	entries, err := os.ReadDir("./matchers/")
 	if err != nil {
 		log.Default().Fatal(err)
 	}
-	defer jsonFile.Close()
-	byteValue, err := io.ReadAll(jsonFile)
-	if err != nil {
-		log.Default().Fatal(err)
+	testableSuggestions := []TestableSuggestions{}
+	for _, e := range entries {
+		jsonFile, err := os.Open(path.Join("./matchers/", e.Name()))
+		if err != nil {
+			log.Default().Fatal(err)
+		}
+		byteValue, err := io.ReadAll(jsonFile)
+		if err != nil {
+			log.Default().Fatal(err)
+		}
+		suggestion := TestableSuggestions{}
+		err = json.Unmarshal(byteValue, &suggestion)
+		if err != nil {
+			log.Default().Fatal(err)
+		}
+		jsonFile.Close()
+		testableSuggestions = append(testableSuggestions, suggestion)
 	}
-	var testableSuggestions []TestableSuggestions
 	var outputSuggestions []OutputSuggestions
-	err = json.Unmarshal(byteValue, &testableSuggestions)
-	if err != nil {
-		log.Default().Fatal(err)
-	}
 	for _, suggestion := range testableSuggestions {
 		regex, err := regexp.Compile(suggestion.Matcher)
 		if err != nil {
 			log.Default().Fatal(err)
 		}
 		if len(suggestion.TestOK) < 1 || len(suggestion.TestFail) < 1 {
-			log.Default().Fatal("")
+			log.Default().Fatal("No tests...")
 		}
 		for _, testOk := range suggestion.TestOK {
 			if !regex.MatchString(testOk) {
